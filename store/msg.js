1393acaa85c001f251f6c099242103b1448d73c5
beidaobadechuiyangliu <guest3@163.com>
2020-02-19 18:18:30 +0800

    新增关于我们和设置页面

---
+++ store/msg.js
@@ -0,0 +1,24 @@
+
+//聊天缓存
+export default {
+	mapFirstMsg: new Map(),	//好友聊天数据
+	mapGroupMsg: new Map(),	//群聊天数据
+	
+	//取好友消息
+	getFriendMsg(friendAccount)
+	{
+		if (this.mapFirstMsg.has(friendAccount))
+			return mapFirstMsg.get(friendAccount);
+			
+		return null;
+	}
+	
+	//保存好友消息
+	setFriendMsg(friendAccount, msg)
+	{
+		if (this.mapFirstMsg.has(friendAccount))
+			return mapFirstMsg.get(friendAccount);
+			
+		return null;
+	}
+}
