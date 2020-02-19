2dc5a18be8fcc3419156cdde61f1fc3aed1694d9
leidianwudi <email>
2020-02-19 20:49:32 +0800

    添加消息设置

--- pages/user/My/about/about.vue
+++ pages/user/My/about/about.vue
@@ -1,17 +1,83 @@
 <template>
-	<view class="content">
+	<view class="content" style="padding:0;">
 		<view class="app_icon">
-			
+			<view class="logo">
+				<image src="/static/img/im.png" class="logo_img" mode="widthFix"></image>
+			</view>
 		</view>
 		<view class="app_info">
-			<view class="app_name"></view>
-			<view class="app_name"></view>			
+			<view class="app_name">im</view>
+			<view class="app_edition">Version 1.0.0</view>			
 		</view>		
+		
+		<view class="func">
+			
+			<view class="func_box">
+		        <view class="">
+		        	<text class="func_test">版本更新</text>
+		        </view>
+				<view class="">
+					<micon type="arrowright" size=18></micon>
+				</view>
+			</view>						
+		</view>
 	</view>
 </template>
 
 <script>
+import micon from '@/components/m-icon/m-icon';
+export default{
+	components: {
+		micon
+	}
+}
 </script>
 
 <style>
+	.app_icon{
+		display:flex;
+		justify-content: center;
+		align-items:center;
+		height:450rpx;
+		width:100%;
+	}
+	.logo{
+		width:250rpx;
+		height:250rpx;
+		border-radius:10px;
+		overflow:hidden;
+	}
+	.logo_img{
+		width:100%;
+		height:100%;
+	}
+	.app_info{
+		display:flex;
+		flex-direction: column;
+		justify-content:center;
+		align-items:center;
+	}
+	.app_name{
+		color:#000;
+		font-size:24px;
+		font-weight:bold;
+	}
+	.app_edition{
+		color:#000;
+		font-size:20px;		
+	}
+	.func{
+		background:#fff;
+		margin-top:100rpx;		
+	}
+	.func_box{
+		border-bottom: 1px solid #F0F0F0;
+		padding:20rpx 50rpx;
+		display:flex;
+		justify-content: space-between;
+		align-items:center;
+	}
+	.func_test{
+		margin-left:20rpx;
+	}
 </style>
--- pages/user/My/setUp/setUp.vue
+++ pages/user/My/setUp/setUp.vue
@@ -1,11 +1,90 @@
 <template>
-	<view class="content">
-		这是设置
+	<view class="content" style="background-color: #F2F2F2; padding:0;">
+		<view class="func">
+			<view class="func_box">
+		        <view class="">
+		        	<text class="func_test">接收新消息通知</text>
+		        </view>
+				<view class="">
+					<switch @change="isNotice" checked color="#39B54A"></switch>
+				</view>
+			</view>
+			
+			<view class="func_box">
+		        <view class="">
+		        	<text class="func_test">声音</text>	
+		        </view>			
+				<view class="">
+					<switch @change="isVoice" checked color="#39B54A"></switch>
+				</view>
+			</view>
+			
+			<view class="func_box">
+			    <view class="">
+			    	<text class="func_test">震动</text>	
+			    </view>			
+				<view class="">
+					<switch @change="isShake" checked color="#39B54A"></switch>
+				</view>
+			</view>
+			
+			<view class="func_box">
+			    <view class="">
+			    	<text class="func_test">消息推送设置</text>	
+			    </view>			
+				<view class="">
+				</view>
+			</view>
+			
+			<view class="func_box">
+			    <view class="">
+			    	<text class="func_test">系统通知</text>	
+			    </view>			
+				<view class="">
+				</view>
+			</view>
+						
+		</view>
 	</view>
 </template>
 
 <script>
+	export default{
+		data() {
+			return {
+				notice: true,
+				voice: true,
+				shake: true
+			}
+		},
+		methods:{
+			isNotice(e) {
+				this.notice = e.detail.value;
+			},
+			isVoice(e) {
+				this.voice = e.detail.value;
+			},
+			isShake(e) {
+				this.shake = e.detail.value;
+			}
+		}
+	}
 </script>
 
-<style>
+<style>	
+	.func{
+		margin-top:30rpx;		
+	}
+	.func_box{
+		background:#fff;
+		padding:40rpx 50rpx;
+		display:flex;
+		justify-content: space-between;
+		align-items:center;
+		margin-bottom:30rpx;
+		border-radius:20px;
+	}
+	.func_test{
+		color:#707070;
+	}
 </style>
